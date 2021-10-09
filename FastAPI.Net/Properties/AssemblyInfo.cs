d9b1d12914a1c1465ce77b3341734a2705479424
Faisal Alsajjan <fta.sajjan@gmail.com>
2021-10-09 04:17:33 +0300

    Nuget packing

--- FastAPI.Net/Properties/AssemblyInfo.cs
+++ FastAPI.Net/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FastAPI.Net")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A stanad alone API creation library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Faisal Alsajjan")]
 [assembly: AssemblyProduct("FastAPI.Net")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
